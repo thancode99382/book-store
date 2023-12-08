486d8d2fb77ee61b9e33b22861efa54089e65979
thancode99382 <dat99382@donga.edu.vn>
2023-12-08 20:55:20 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/book-store/'
+  }
+
+  return config
 })
