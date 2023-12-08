48b719aae737f6fc4afb93f6890a2d9c46243209
thancode99382 <dat99382@donga.edu.vn>
2023-12-08 21:06:33 +0700

    .

--- src/components/Cart/CardReview.jsx
+++ src/components/Cart/CardReview.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
-import "../../assets/css/Product-Viewer-1-untitled.css";
-import "../../assets/js/Product-Viewer-1-3.1.1-jquery.min.js";
-import "../../assets/js/Product-Viewer-1-untitled.js";
+import "/public/css/Product-Viewer-1-untitled.css";
+import "/public/js/Product-Viewer-1-3.1.1-jquery.min.js";
+import "/public/js/Product-Viewer-1-untitled.js";
 
 export default function CardReview({ image }) {
   return (
